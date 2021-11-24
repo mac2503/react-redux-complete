6937047869b3a7c9b3618f6a17a1b2eecc2f8649
Mahek Khowala <mahekkhowala@gmail.com>
2021-11-24 18:03:09 +0530

    create react app

---
+++ myapp/src/App.js
@@ -0,0 +1,13 @@
+import Ninjas from './Ninjas';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My first React app!</h1>
+      <p>Welcome :)</p>
+      <Ninjas />
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Ninjas extends Component {
+  render() {
+    return(
+      <div className="ninja">
+      <div>Name: Mahek</div>
+      <div>Age: 19</div>
+      <div>Belt: Black</div>
+      </div>
+    )
+  }
+}
+
+export default Ninjas
